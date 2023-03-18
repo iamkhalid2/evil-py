40c80f0632ec732073daae84f09e6d0d4571cdae
Khalid <mkhalid2829@gmail.com>
2023-03-18 22:03:37 +0530

    Update python code.py

--- python code.py
+++ python code.py
@@ -1,5 +1,11 @@
 import time
 import pyautogui
 
-pyautogui.write('Your Message Here')
-pyautogui.press('enter')
+time.sleep(5) # number of seconds you want to wait before the script starts
+
+while True:
+    pyautogui.write('Your message here') 
+    pyautogui.press('enter')
+    time.sleep(0.1)   # Replace 0.1 with the number of seconds you want to wait between key presses
+
+# Move the cursor to the top left corner of the screen to stop the script (Fail safe)
